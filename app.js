390f6db1ae9d5e7ae1b08ff13270a20b30cd1ee5
Natan Utevsky <99504834+nathutev@users.noreply.github.com>
2023-01-15 11:15:49 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ let dotY;
 let result;
 let ansLabel = document.getElementById("ansLabel");
 let errorLabel = document.getElementById("errorLabel");
+let checkbox = document.getElementById("checkboxButton");
 
 function solve(){
   errorLabel.innerHTML = "";
@@ -14,69 +15,144 @@ function solve(){
   y = document.getElementById("y").value;
   dotY = document.getElementById("dotY").value;
 
-  if(x == "" || dotX == "" || y == "" || dotY == ""){
-    errorLabel.innerHTML = "Enter number/numbers";
-  }
+  if(checkbox.checked === false){
 
-  else if(x === "x"){      
-    dotX = Number(dotX);
-    y = Number(y);
-    dotY = Number(dotY);    
-    result = y * dotX / dotY;
-
-    if(isNaN(result)){
-      errorLabel.innerHTML = "some kind of error, idk check your numbers"
+    if(x == "" || dotX == "" || y == "" || dotY == ""){
+      errorLabel.innerHTML = "Enter number/numbers";
     }
-   
+
+    else if(x === "x"){
+      dotX = Number(dotX);
+      y = Number(y);
+      dotY = Number(dotY);    
+      result = y * dotX / dotY;
+
+      if(isNaN(result)){
+        errorLabel.innerHTML = "some kind of error, idk check your numbers"
+      }
+    
+      else{
+        ansLabel.innerHTML = result;
+      };
+    }
+
+    else if(dotX === "x"){
+      x = Number(x);
+      y = Number(y);
+      dotY = Number(dotY);
+      result = x * dotY / y;
+
+      if(isNaN(result)){
+        errorLabel.innerHTML = "not a number"
+      }
+
+      else{
+        ansLabel.innerHTML = result;
+      };
+    }
+
+    else if(y === "x"){
+      x = Number(x);
+      dotX = Number(dotX);
+      dotY = Number(dotY);
+      result = x * dotY / dotX;
+      if(isNaN(result)){
+        errorLabel.innerHTML = "not a number"
+      }
+
+      else{
+        ansLabel.innerHTML = result;
+      };
+    }
+
+    else if(dotY === "x"){
+      x = Number(x);
+      dotX = Number(dotX);
+      y = Number(y);
+      result = y * dotX / x;
+      if(isNaN(result)){
+        errorLabel.innerHTML = "not a number"
+      }
+
+      else{
+        ansLabel.innerHTML = result;
+      };
+    }
+
     else{
-      ansLabel.innerHTML = result;
+      errorLabel.innerHTML = "use <b>x</b> for variable"
     };
   }
 
-  else if(dotX === "x"){
-    x = Number(x);
-    y = Number(y);
-    dotY = Number(dotY);
-    result = x * dotY / y;
+  else if(checkbox.checked === true){
+    
+    if(x == "" || dotX == "" || y == "" || dotY == ""){
+      errorLabel.innerHTML = "Enter number/numbers";
+    }
 
-    if(isNaN(result)){
-      errorLabel.innerHTML = "not a number"
+    else if(x === "x"){
+      dotX = Number(dotX);
+      y = Number(y);
+      dotY = Number(dotY);    
+      result = y * dotY / dotX;
+
+      if(isNaN(result)){
+        errorLabel.innerHTML = "some kind of error, idk check your numbers"
+      }
+    
+      else{
+        ansLabel.innerHTML = result;
+      };
+    }
+
+    else if(dotX === "x"){
+      x = Number(x);
+      y = Number(y);
+      dotY = Number(dotY);
+      result = x * y / dotY;
+
+      if(isNaN(result)){
+        errorLabel.innerHTML = "not a number"
+      }
+
+      else{
+        ansLabel.innerHTML = result;
+      };
+    }
+
+    else if(y === "x"){
+      x = Number(x);
+      dotX = Number(dotX);
+      dotY = Number(dotY);
+      result = x * dotX / dotY;
+
+      if(isNaN(result)){
+        errorLabel.innerHTML = "not a number"
+      }
+
+      else{
+        ansLabel.innerHTML = result;
+      };
+    }
+
+    else if(dotY === "x"){
+      x = Number(x);
+      dotX = Number(dotX);
+      y = Number(y);
+      result = y * x / dotX;
+
+      if(isNaN(result)){
+        errorLabel.innerHTML = "not a number"
+      }
+
+      else{
+        ansLabel.innerHTML = result;
+      };
     }
 
     else{
-      ansLabel.innerHTML = result;
+      errorLabel.innerHTML = "use <b>x</b> for variable"
     };
+
   }
-
-  else if(y === "x"){
-    x = Number(x);
-    dotX = Number(dotX);
-    dotY = Number(dotY);
-    result = x * dotY / dotX;
-    if(isNaN(result)){
-      errorLabel.innerHTML = "not a number"
-    }
-
-    else{
-      ansLabel.innerHTML = result;
-    };
-  }
-
-  else if(dotY === "x"){
-    x = Number(x);
-    dotX = Number(dotX);
-    y = Number(y);
-    result = y * dotX / x;
-    if(isNaN(result)){
-      errorLabel.innerHTML = "not a number"
-    }
-
-    else{
-      ansLabel.innerHTML = result;
-    };
-  }
-
-  else{
-    errorLabel.innerHTML = "use <b>x</b> for variable"
-  };
 };
